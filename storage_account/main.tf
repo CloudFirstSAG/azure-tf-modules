27c50966e1323178f8001884e09fe475657e5abf
Mohamed Ghaleb <mohamed.ghaleb@gmail.com>
2016-12-10 21:42:20 +0100

    Minor Updates

--- storage_account/main.tf
+++ storage_account/main.tf
@@ -8,7 +8,7 @@ variable location {
 }
 
 variable account_name {
-  description = "Storage account name. Must be unique across Azure." 
+  description = "Storage account name. Must be unique across Azure."
 }
 
 variable account_type {
@@ -27,6 +27,6 @@ output "primary_blob_endpoint" {
     value = "${azurerm_storage_account.sa.primary_blob_endpoint}"
 }
 
-output "name" {
-    value = "${azurerm_storage_account.sa.name}"
-}
+#output "name" {
+#    value = "${azurerm_storage_account.sa.name}"
+#}
