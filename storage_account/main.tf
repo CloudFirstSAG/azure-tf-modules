5aa7a2677ac28009ef8b1d55c6a289bb947f7f36
Mohamed Ghaleb <mohamed.ghaleb@gmail.com>
2016-12-10 21:44:08 +0100

    Minor Updates

--- storage_account/main.tf
+++ storage_account/main.tf
@@ -17,16 +17,16 @@ variable account_type {
 }
 
 resource "azurerm_storage_account" "sa" {
-    name = "${var.account_name}"
-    resource_group_name = "${var.resource_group_name}"
-    location = "${var.location}"
-    account_type = "${var.account_type}"
+  name = "${var.account_name}"
+  resource_group_name = "${var.resource_group_name}"
+  location = "${var.location}"
+  account_type = "${var.account_type}"
 }
 
 output "primary_blob_endpoint" {
-    value = "${azurerm_storage_account.sa.primary_blob_endpoint}"
+  value = "${azurerm_storage_account.sa.primary_blob_endpoint}"
 }
 
-#output "name" {
-#    value = "${azurerm_storage_account.sa.name}"
-#}
+output "name" {
+  value = "${azurerm_storage_account.sa.name}"
+}
